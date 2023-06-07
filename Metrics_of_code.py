277180c9f3ba2bcaa1da95bafb8ffbd3b20c5992
Aziza-Nurimova <mirunchik18@gmail.com>
2023-06-07 21:43:08 +0500

    Initial commit

---
+++ Metrics_of_code.py
@@ -0,0 +1,22 @@
+import numpy as np
+from guess_number import random_predict
+from Smart_guess_number import game_core_v2
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+score_game(random_predict)
+score_game(game_core_v2)
---
+++ Smart_metrics_code.py
@@ -0,0 +1,30 @@
+import numpy as np
+from guess_number import random_predict
+from Smart_guess_number import game_core_v2
+from Metrics_of_code import score_game
+def game_core_v3(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, 
+    затем сужаем диапазон с обеих сторон, в зависимости от результата сравнения переменных, 
+далее определяем предполагаемое число путем деления диапазона по полам, до тех пор пока не угадаем число.
+    Функция принимает загаданное число и возвращает число попыток
+       
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+    left_border=1
+    right_border=101
+    while number!=predict:
+        count+=1
+        predict=left_border+(right_border-left_border)//2
+        if predict > number:  right_border=predict
+        elif  predict < number: left_border=predict
+        else: break
+        
+    return count
+
+score_game(game_core_v3)
